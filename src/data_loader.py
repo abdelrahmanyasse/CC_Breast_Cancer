7c68a5b9c058dcca5bfd811b3ed0e7d64a6e237c
Abdelrahman Yasser <abdelrahmanyasser157@gmail.com>
2026-06-06 21:37:01 +0300

    iris dataset function load

---
+++ src/data_loader.py
@@ -0,0 +1,8 @@
+import sklearn.datasets
+import pandas as pd
+
+def load_data():
+    dataset = sklearn.datasets.load_breast_cancer()
+    df = pd.DataFrame(dataset.data, columns = dataset.feature_names)
+    
+    return dataset
