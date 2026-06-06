4f9e9fcd6fef6c6ba83663241b5f401db8b7c8b3
Abdelrahman Yasser <abdelrahmanyasser157@gmail.com>
2026-06-06 22:27:21 +0300

    Training plots

---
+++ src/visualizations.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+def Accuracy_loss_visualization(Training):
+    fig = plt.figure(figsize=(12, 4))
+    
+    plt.plot(Training.history['accuracy'])
+    plt.plot(Training.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(["training data",'validations data'], loc = 'lower right')
+    plt.show()
+    return fig
+    
+def Accuracy_epochs_visualization(Training):
+    fig = plt.figure(figsize=(12, 4))
+    
+    plt.plot(Training.history['loss'])
+    plt.plot(Training.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(["training data",'validations data'], loc = 'upper right')
+    plt.show()
+    return fig
