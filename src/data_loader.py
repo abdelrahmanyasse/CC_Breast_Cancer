25dfe007e800825a86ddf3f27bed99c8e8e30ede
Abdelrahman Yasser <abdelrahmanyasser157@gmail.com>
2026-06-07 17:04:29 +0300

    target Class added to df

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,4 +4,5 @@ import pandas as pd
 def load_data():
     dataset = sklearn.datasets.load_breast_cancer()
     df = pd.DataFrame(dataset.data, columns = dataset.feature_names)
+    df['Class'] = dataset.target
     return df
