1e60a0a800a94c79b801e60d4df52f5e016968aa
Abdelrahman Yasser <abdelrahmanyasser157@gmail.com>
2026-06-07 17:11:30 +0300

    .

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,7 +5,7 @@ def preprocessing(df):
     X = df.drop('Class', axis = 1)
     y = df['Class']
     
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.25,random_state = 42)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
     
     Scaler  = StandardScaler()
     X_train_std  = Scaler.fit_transform(X_train)
