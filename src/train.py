3712b5864cf6ff6487bbee2cb348f46292d0e580
Abdelrahman Yasser <abdelrahmanyasser157@gmail.com>
2026-06-07 17:10:19 +0300

    parameters passing changes

--- src/train.py
+++ src/train.py
@@ -1,9 +1,8 @@
-def Optimize_model(model,X_train_std,y_train):
+def Optimize_model(model,X_train_std,y_train,epochs):
     model.compile(
         optimizer = 'adam',
         loss = 'sparse_categorical_crossentropy',
         metrics = ['accuracy']
         )
-    Training = model.fit(X_train_std,y_train,validation_split=0.1,epochs = 10)
-    
+    Training = model.fit(X_train_std,y_train,validation_split=0.1,epochs = epochs)
     return Training
